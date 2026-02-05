ad017cb874fbf6efc4413c956445f5d1507a978a
Tawhid-01 <mtworld008@gmail.com>
2026-02-05 22:18:26 +0600

    CRUD operation

--- src/app.js
+++ src/app.js
@@ -15,4 +15,16 @@ app.get('/notes', (req, res) => {
   res.status(200).json({message: 'Notes get successfully', notes})
 })
 
+app.delete('/notes/:index', (req, res) => {
+  const index = req.params.index;
+ notes.splice(index, 1);
+  res.status(200).json({message: 'Note deleted successfully'})
+})
+
+app.patch('/notes/:index', (req, res) => {
+  const index = req.params.index;
+  const updatedNote = req.body;
+  notes[index] = updatedNote;
+  res.status(200).json({message: 'Note updated successfully'})
+})
 module.exports = app;
